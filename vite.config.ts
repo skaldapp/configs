65e8cbdd8caafda54d3250ef2fd54300714c78b6
Jerry Bruwes <jbruwes@icloud.com>
2026-01-14 14:23:56 +0200

    refactor: remove reactivity-transform plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import unoCSS from "@unocss/vite";
 import vue from "@vitejs/plugin-vue";
-import reactivityTransform from "@vue-macros/reactivity-transform/vite";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -10,11 +9,5 @@ export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(process.env["npm_package_version"]),
   },
-  plugins: [
-    reactivityTransform(),
-    vue(),
-    vueDevTools(),
-    unoCSS(),
-    tsconfigPaths(),
-  ],
+  plugins: [vue(), vueDevTools(), unoCSS(), tsconfigPaths()],
 });
