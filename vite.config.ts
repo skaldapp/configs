fb4bd0a0f2234668f282b6703190cc3bf9d48bbe
Jerry Bruwes <jbruwes@icloud.com>
2026-03-13 12:55:27 +0200

    Update dependencies including vite and @vitejs/plugin-vue

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import unoCSS from "@unocss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 const base = "./",
   define = {
     __APP_VERSION__: JSON.stringify(process.env["npm_package_version"]),
   },
-  plugins = [vue(), vueDevTools(), unoCSS(), tsconfigPaths()];
+  plugins = [vue(), vueDevTools(), unoCSS()],
+  tsconfigPaths = true,
+  resolve = { tsconfigPaths };
 
-export default defineConfig({ base, define, plugins });
+export default defineConfig({ base, define, plugins, resolve });
