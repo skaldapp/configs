263ae9dc147a5595563b2b5cea3ccc0f80f3dd02
Jerry Bruwes <jbruwes@gmail.com>
2026-08-02 17:32:43 +0200

    chore: update prettier config with jsonc support

--- prettierrc.config.ts
+++ prettierrc.config.ts
@@ -1,17 +1,20 @@
 import type { Config } from "prettier";
 
 const endOfLine = "auto",
+  files = ["*.jsonc"],
   jsdocCommentLineStrategy = "keep",
   plugins = [
     "@prettier/plugin-pug",
     "prettier-plugin-jsdoc",
     "prettier-plugin-packagejson",
   ],
+  trailingComma = "none",
   tsdoc = true;
 
 export default {
   endOfLine,
   jsdocCommentLineStrategy,
+  overrides: [{ files, options: { trailingComma } }],
   plugins,
   tsdoc,
 } satisfies Config;
